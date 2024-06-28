42a9d14c71931b7f10aea0fadaa7d88c6a336e71
viditpandey06 <viditpandey06@gmail.com>
2024-06-28 21:29:53 +0530

    Update index.js

--- mern-blog/backend/index.js
+++ mern-blog/backend/index.js
@@ -31,15 +31,15 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/post", postRoutes);
-app.use("/api/comment", commentRoutes);
+app.use("/backend/user", userRoutes);
+app.use("/backend/auth", authRoutes);
+app.use("/backend/post", postRoutes);
+app.use("/backend/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
