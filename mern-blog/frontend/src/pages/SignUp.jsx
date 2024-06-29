582496f07e609a8cc29f66eda3e226db31ba594f
viditpandey06 <viditpandey06@gmail.com>
2024-06-29 21:32:20 +0530

    Update SignUp.jsx

--- mern-blog/frontend/src/pages/SignUp.jsx
+++ mern-blog/frontend/src/pages/SignUp.jsx
@@ -26,6 +26,7 @@ export default function SignUp() {
       });
       const data = await res.json();
       if (data.success === false) {
+       console.log("error here1");
         return setErrorMessage(data.message);
       }
       setLoading(false);
@@ -44,7 +45,7 @@ export default function SignUp() {
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
+              VIDIT's
             </span>
             Blog
           </Link>
