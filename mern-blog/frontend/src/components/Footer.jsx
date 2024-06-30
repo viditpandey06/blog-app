c1a9f76cdd292062d02b3f9ac3aa02f9abbd7cc7
viditpandey06 <viditpandey06@gmail.com>
2024-06-30 14:14:53 +0530

    Update Footer.jsx

--- mern-blog/frontend/src/components/Footer.jsx
+++ mern-blog/frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
-import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble, BsBasket, BsMailbox } from 'react-icons/bs';
 export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
@@ -12,7 +12,7 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sahand's
+                VIDIT's
               </span>
               Blog
             </Link>
@@ -22,18 +22,18 @@ export default function FooterCom() {
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.100jsprojects.com'
+                  href='https://portfolio-website-h7wx.vercel.app/'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  100 JS Projects
+                  About Me
                 </Footer.Link>
                 <Footer.Link
                   href='/about'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand's Blog
+                  VIDIT's Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -41,7 +41,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://www.github.com/viditpandey06'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
@@ -63,16 +63,15 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Sahand's blog"
+            by="VIDIT's blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-            <Footer.Icon href='#' icon={BsFacebook}/>
-            <Footer.Icon href='#' icon={BsInstagram}/>
-            <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/>
-            <Footer.Icon href='#' icon={BsDribbble}/>
-
+            <Footer.Icon href='#' icon={BsFacebook} />
+            <Footer.Icon href='#' icon={BsInstagram} />
+            <Footer.Icon href='#' icon={BsTwitter} />
+            <Footer.Icon href='https://github.com/viditpandey06' icon={BsGithub} />
+            <Footer.Icon href='#' icon={BsMailbox} />
           </div>
         </div>
       </div>
